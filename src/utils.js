bcd02836ec24865833981555e33385fdb4738683
wangyuqi <494849261@qq.com>
2022-11-13 18:06:43 +0800

    feat: use MutationObserver create micro task in browser

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,16 @@
 const isFunction = (fn) => typeof fn === "function";
 const isObject = (x) => x && typeof x === "object";
-const createMicroTask = (fn) => setTimeout(fn, 0);
+const createMicroTask = (fn) => {
+  const isBrowser =
+    typeof window !== "undefined" && typeof window.document !== "undefined";
+
+  if (isBrowser && typeof MutationObserver !== "undefined") {
+    const observer = new MutationObserver(fn);
+    const node = document.createTextNode("");
+    observer.observe(node, { characterData: true });
+    node.data = "trigger";
+  } else setTimeout(fn, 0);
+};
 const createMicroTasks = (tasks) => {
   while (tasks.length) {
     createMicroTask(tasks.shift());
